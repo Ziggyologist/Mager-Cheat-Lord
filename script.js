cc914b0b306cf239f11f65f60e49ed0981bf2f07
Ziggyologist <teodora11vela@gmail.com>
2022-08-08 17:55:34 +0300

    added testing prototype

---
+++ script.js
@@ -0,0 +1,9 @@
+const photoBtn = document.querySelectorAll(".story_img");
+const storyContainer = document.querySelectorAll(".story_container");
+
+photoBtn.forEach((btn, i) => {
+  btn.addEventListener("click", function () {
+    console.log(storyContainer[i]);
+    storyContainer[i].style.height = "400px";
+  });
+});
