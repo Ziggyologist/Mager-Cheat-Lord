5ceef41886cc7bf5ae756b61cd0d4013ba2af8ca
Ziggyologist <teodora11vela@gmail.com>
2022-08-08 17:58:34 +0300

    added timer

--- script.js
+++ script.js
@@ -5,5 +5,8 @@ photoBtn.forEach((btn, i) => {
   btn.addEventListener("click", function () {
     console.log(storyContainer[i]);
     storyContainer[i].style.height = "400px";
+    setTimeout(function () {
+      storyContainer[i].style.height = "200px";
+    }, 18000);
   });
 });
